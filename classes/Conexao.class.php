491ae4c37377c7ca8e9144e714485b4f2d1be171
Guto Ribeiro <gutofunny@yahoo.com.br>
2015-11-22 13:05:06 -0300

    Inclusão de array com opções para uso no PDO de conexão, set char set UTF8

--- classes/Conexao.class.php
+++ classes/Conexao.class.php
@@ -1,14 +1,17 @@
 <?php
 require_once 'Config.php';
 
-abstract class Conexao {
+require_once 'Funcoes.class.php';
+
+abstract class Conexao extends Funcoes {
 
     private static $instance;
 
     private static function conectar() {
         try {
             if (self::$instance == null) :
-                self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+                $opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8');
+                self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $opcoes);
                 self::$instance -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$instance -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             endif;
